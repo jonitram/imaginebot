f8443934c57e123fdaac8f54ddeed17593e8b9ac
jonitram <jon.l.nitram@gmail.com>
2020-03-05 13:43:46 -0600

    tested accessing at a particular index

--- imaginebot.py
+++ imaginebot.py
@@ -1,10 +1,17 @@
 #!/usr/local/bin/python3.7
-import tweepy
-import logging
-from config import create_api
+import config.py
+
+word = None
 
 def main():
-    print("working")
+    with open("words_alpha.txt") as fp:
+        for i, line in enumerate(fp):
+            if i == index:
+                word = line.strip()
+    if word != None:
+        # things
+    else:
+        # bot is done!
 
 if __name__ == "__main__":
     main()
