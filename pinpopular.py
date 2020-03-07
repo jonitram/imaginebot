2424e5d325611aa219cea1ffa8a116495a91b951
jonitram <jon.l.nitram@gmail.com>
2020-03-07 14:09:41 -0600

    made logic more strict on when it sends me updates

--- pinpopular.py
+++ pinpopular.py
@@ -1,19 +1,22 @@
-#!/usr/local/bin/python3.7
+#!/usr/local/bin/python3.8
 import config
 
 # run every 8 hours
 
-# return tweet
 def get_recent_tweet(client):
     max_likes = None
     for tweet in client.user_timeline():
         if max_likes == None:
             max_likes = tweet
-        elif tweet.favorite_count >= max_likes.favorite_count:
+        elif tweet.favorite_count > max_likes.favorite_count:
             max_likes = tweet
     return max_likes
 
-def update_pin(client, tweet):
+def update_pin(client, tweet, output_file):
+    personal_account_id = config.os.getenv("PERSONAL_ACCOUNT_ID")
+    status_update = "New most liked tweet: \"" + tweet.text + "\" with: " + str(tweet.favorite_count) + " likes"
+    client.send_direct_message(personal_account_id, status_update)
+    config.save_pickle(output_file, tweet.id)
     return
 
 
@@ -30,19 +33,10 @@ def main():
         # have a most liked tweet from before
         if recent_liked != None:
             # have recently liked tweet
-            if recent_liked.favorite_count >= client.get_status(id=most_liked_tweet_id).favorite_count:
-                # new most_liked_tweet
-                # dm to me
-                # pickle it
-            else:
-                # comparison failed so don't send dm
-        else:
-            # do not have recently liked tweet so no comparison can be done
-            # don't send dm
+            if recent_liked.favorite_count > client.get_status(id=most_liked_tweet_id).favorite_count:
+                update_pin(client, recent_liked, most_liked_file)
     elif recent_liked != None:
-        # recently liked tweet is the new most_liked_tweet
-        # send dm
-        # pickle it
+        update_pin(client, recent_liked, most_liked_file)
     return
 
 if __name__ == "__main__":
