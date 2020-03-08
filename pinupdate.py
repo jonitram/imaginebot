14bf9c98c7ea3606e2d603ddf8af3b8306bc673f
jonitram <jon.l.nitram@gmail.com>
2020-03-07 18:57:31 -0600

    spamon

--- pinupdate.py
+++ pinupdate.py
@@ -1,25 +1,20 @@
 #!/usr/local/bin/python3.7
 import config
 
-# run once a day
+# run every 4 hours
 
 def get_recent_tweet(client):
     max_likes = None
     for tweet in client.user_timeline():
         if max_likes == None:
             max_likes = tweet
-        elif tweet.favorite_count > max_likes.favorite_count:
+        elif tweet.favorite_count >= max_likes.favorite_count:
             max_likes = tweet
     return max_likes
 
-def get_last_tweet(client):
-    for direct_message in client.list_direct_messages():
-        if direct_message.user_id == int(config.os.getenv("PERSONAL_ACCOUNT_ID"))
-            return direct_message.id
-
-def update_pin(client, tweet, output_file):
+def update_pin(client, tweet):
     personal_account_id = int(config.os.getenv("PERSONAL_ACCOUNT_ID"))
-    status_update = "New most liked tweet: \"" + tweet.text + "\" with: " + str(tweet.favorite_count) + " likes"
+    status_update = "Most liked tweet update: \"" + tweet.text + "\" with: " + str(tweet.favorite_count) + " likes"
     client.send_direct_message(personal_account_id, status_update)
     return
 
@@ -28,11 +23,10 @@ def lambda_handler(event, context):
     return
 
 def main():
-    
     client = config.login()
-
     recent_liked = get_recent_tweet(client)
-
+    if recent_liked:
+        update_pin(client, recent_liked)
     return
 
 if __name__ == "__main__":
